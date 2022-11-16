5b6f17e0f2274c44254a8c3e4fe9c89eb5b06d8d
Gabriel Gualberto <gabrielgualbertooliveira@gmail.com>
2022-11-16 20:04:17 -0300

    feat: Exercicio pipe customizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,19 +6,32 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
 import { TabelasComponent } from './pages/tabelas/tabelas.component';
+import { FormataTelefonePipe } from './pipes/formata-telefone.pipe';
+import { FormataCpfPipe } from './pipes/formata-cpf.pipe';
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
 
+registerLocaleData(ptBr);
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SobreComponent,
-    TabelasComponent
+    TabelasComponent,
+    FormataTelefonePipe,
+    FormataCpfPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
